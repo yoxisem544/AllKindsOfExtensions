3977241c27c80a699d1d6b629095e976600293e0
David Lin <yoxisem544@gmail.com>
2016-12-12 15:46:41 +0800

    extension of table view

--- AllKindsExtensions/UITableViewExtension.swift
+++ AllKindsExtensions/UITableViewExtension.swift
@@ -22,4 +22,12 @@ extension UITableView {
         }
     }
     
+    func register<T: UITableViewCell>(cellTypes: [T.Type]) {
+        cellTypes.forEach { register(cellType: $0) }
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(with type: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withIdentifier: type.className, for: indexPath) as! T
+    }
+    
 }
