e40043c89464a952f6d2d8b53ef5509c4c4733c0
David Lin <yoxisem544@gmail.com>
2016-12-12 16:10:17 +0800

    add uilabel extension

--- AllKindsExtensions/UILabelExtension.swift
+++ AllKindsExtensions/UILabelExtension.swift
@@ -11,15 +11,25 @@ import UIKit
 
 extension UILabel {
     
-	/// Get preferred text width according to font size.
-	func preferredTextWidthConstraintByFontSize() -> CGFloat {
-		guard let text = self.text else { return 0 }
-		return text.preferredTextWidth(constraintByFontSize: self.font.pointSize)
-	}
-	
-	/// Update label's width according to its font size and its text.
-	func updateWidthToPreferredSizeByFontSize() {
-		self.frame.size.width = self.preferredTextWidthConstraintByFontSize()
-	}
+    func changeFontSize(_ size: CGFloat) -> Self {
+        guard let font = self.font else { return self }
+        self.font = UIFont.init(name: font.fontName, size: size)
+        return self
+    }
+    
+    func changeTextColor(_ textColor: UIColor) -> Self {
+        self.textColor = textColor
+        return self
+    }
+    
+    func changeTextAlignment(_ alignment: NSTextAlignment) -> Self {
+        self.textAlignment = alignment
+        return self
+    }
+    
+    func changeNumberOfLines(_ lines: Int) -> Self {
+        self.numberOfLines = lines
+        return self
+    }
     
 }
